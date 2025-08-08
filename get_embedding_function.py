bb5e8d8e2157be7c22f366ecdf70d72f3512d1f2
SrijanKathel <srijan.kathel@gmail.com>
2025-08-08 17:01:23 +0530

    Add files via upload

---
+++ get_embedding_function.py
@@ -0,0 +1,8 @@
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+
+
+def get_embedding_function():
+    #embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    embeddings = OllamaEmbeddings(model="mxbai-embed-large")
+
+    return embeddings
